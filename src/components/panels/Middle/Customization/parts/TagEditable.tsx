9e4fde5b987d6f00a0e75815644773389302acca
danielx-art <metacarpo10@gmail.com>
2023-11-23 13:56:18 -0300

    custom enter icon

--- src/components/panels/Middle/Customization/parts/TagEditable.tsx
+++ src/components/panels/Middle/Customization/parts/TagEditable.tsx
@@ -79,7 +79,7 @@ const TagEditable: React.FC<PropsType> = ({ store, thisEntity, thisTag }) => {
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className="h-6 w-6"
+            className="h-4 w-4"
           >
             <path
               strokeLinecap="round"
