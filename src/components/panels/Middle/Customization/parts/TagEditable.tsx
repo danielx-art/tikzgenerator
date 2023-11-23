36b1aac8d832b35d379ba73dbf2814822e24ebb9
danielx-art <metacarpo10@gmail.com>
2023-11-23 13:55:57 -0300

    custom enter icon

--- src/components/panels/Middle/Customization/parts/TagEditable.tsx
+++ src/components/panels/Middle/Customization/parts/TagEditable.tsx
@@ -2,6 +2,7 @@ import { type Action, type State } from "import/utils/store";
 import { type Tentity, type Tetiqueta } from "public/entidades";
 import { ChangeEvent, KeyboardEvent, useState } from "react";
 import { updateTag } from "import/utils/miscEntity";
+import EnterIconSvg from "import/components/micro/EnterIconSVG";
 
 type PropsType = {
   store: State & Action;
@@ -49,26 +50,29 @@ const TagEditable: React.FC<PropsType> = ({ store, thisEntity, thisTag }) => {
       />
       <button onClick={handleBtnPress}>
         {editMode ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 6h.008v.008H6V6z"
-            />
-          </svg>
+          <div className="h-4 w-4">
+            <EnterIconSvg />
+          </div>
         ) : (
+          // <svg
+          //   xmlns="http://www.w3.org/2000/svg"
+          //   fill="none"
+          //   viewBox="0 0 24 24"
+          //   strokeWidth="1.5"
+          //   stroke="currentColor"
+          //   className="h-6 w-6"
+          // >
+          //   <path
+          //     strokeLinecap="round"
+          //     strokeLinejoin="round"
+          //     d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
+          //   />
+          //   <path
+          //     strokeLinecap="round"
+          //     strokeLinejoin="round"
+          //     d="M6 6h.008v.008H6V6z"
+          //   />
+          // </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
