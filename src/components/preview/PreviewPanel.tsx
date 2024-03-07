b886fdcaf61a774a386e4b043924f814209e50ad
danielx-art <metacarpo10@gmail.com>
2024-03-07 10:21:43 -0300

    fix circles getting out of screen

--- src/components/preview/PreviewPanel.tsx
+++ src/components/preview/PreviewPanel.tsx
@@ -45,26 +45,43 @@ const PreviewPanel = () => {
       maxY = Math.max(maxY, point.coords.y * RES_FACTOR);
     });
 
-    let pointsWidth = maxX - minX;
-    let pointsHeight = maxY - minY;
+    if (store.circles && store.circles.size > 0) {
+      //circles can get out of bounds, so i need to reajust max and min values
+      store.circles.forEach((circle) => {
+        const cLeft = (circle.center.x - circle.radius) * RES_FACTOR;
+        if (cLeft < minX) minX = cLeft;
+
+        const cTop = (circle.center.y + circle.radius) * RES_FACTOR;
+        if (cTop > maxY) maxY = cTop;
+
+        const cRight = (circle.center.x + circle.radius) * RES_FACTOR;
+        if (cRight > maxX) maxX = cRight;
+
+        const cBottom = (circle.center.y - circle.radius) * RES_FACTOR;
+        if (cBottom < minY) minY = cBottom;
+      });
+    }
+
+    let properWidth = maxX - minX;
+    let properHeight = maxY - minY;
 
     let padding = 0.2; //of the maximum dimension
 
     if (pointsArr.length == 1 && pointsArr[0]) {
-      pointsWidth = ((pointsArr[0].size * 1) / 2) * RES_FACTOR;
-      pointsHeight = pointsWidth;
+      properWidth = ((pointsArr[0].size * 1) / 2) * RES_FACTOR;
+      properHeight = properWidth;
       padding = 2;
     }
 
-    const viewAR = pointsWidth / pointsHeight;
+    const viewAR = properWidth / properHeight;
 
     padding =
-      viewAR >= 1 ? (padding *= pointsWidth) : (padding *= pointsHeight);
+      viewAR >= 1 ? (padding *= properWidth) : (padding *= properHeight);
 
     let viewBoxX = +(minX - padding);
     let viewBoxY = -(maxY + padding);
-    let viewBoxWidth = pointsWidth + 2 * padding;
-    let viewBoxHeight = pointsHeight + 2 * padding;
+    let viewBoxWidth = properWidth + 2 * padding;
+    let viewBoxHeight = properHeight + 2 * padding;
 
     const pixelAR = dimensions.width / dimensions.height;
 
