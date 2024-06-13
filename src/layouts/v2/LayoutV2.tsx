8f8f577ad90be16cdb069c30b4199c99fc5f9754
danielx-art <metacarpo10@gmail.com>
2024-06-13 15:45:17 -0300

    refactor switch layout sides, add shadcn pretty scroll

--- src/layouts/v2/LayoutV2.tsx
+++ src/layouts/v2/LayoutV2.tsx
@@ -1,29 +1,31 @@
-
 import Customization from "import/components/custom/Customization";
 import AddPointInput from "import/components/entities/AddPointInput";
 import HeroBar from "import/components/hero/HeroBar";
 import Panel from "import/components/micro/Panel";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "import/components/micro/ui/accordion";
+import { Accordion } from "import/components/micro/ui/accordion";
+import { ScrollArea } from "import/components/micro/ui/scroll-area";
 import PreviewPanel from "import/components/preview/PreviewPanel";
 import Tools from "import/components/tools/Tools";
 
 const LayoutV2 = () => {
   return (
     <>
-      <div className="md:no-scrollbar flex flex-col gap-4 bg-background p-4 font-jost md:grid md:h-full md:max-h-full md:grid-cols-[1fr_2fr]">
-        <Panel className="gap-4 overflow-auto relative">
-          <HeroBar />
-          <Accordion type="multiple" className="w-full">
-            <Customization />
-            <Tools />
-          </Accordion>
-        </Panel>
-        <div className="flex md:flex-col h-[96vh] flex-col-reverse justify-between gap-4 md:h-full">
+      <div className="md:no-scrollbar font-jost flex flex-col gap-4 bg-background p-4 md:grid md:h-full md:max-h-full md:grid-cols-[2fr_1fr]">
+        <div className="flex h-[96vh] flex-col-reverse justify-between gap-4 md:h-full md:flex-col">
           <div className="flex-1">
             <PreviewPanel />
           </div>
           <AddPointInput />
         </div>
+        <Panel className="relative gap-4 overflow-hidden pr-1">
+          <HeroBar />
+          <ScrollArea className="w-full">
+            <Accordion type="multiple" className="w-full pr-3">
+              <Customization />
+              <Tools />
+            </Accordion>
+          </ScrollArea>
+        </Panel>
       </div>
     </>
   );
